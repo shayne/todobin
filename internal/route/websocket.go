e9260de4033b41b985146fb84997be7be2a92f25
Ian Graham <imartingraham@gmail.com>
2019-11-11 20:55:30 -0800

    Added a ping and reconnect on the front end. Refactored websocket.go

--- internal/route/websocket.go
+++ internal/route/websocket.go
@@ -17,8 +17,24 @@ type Message struct {
 	} `json:"data"`
 }
 
-var clients = make(map[string][]*websocket.Conn)
-var broadcast = make(chan Message) // broadcast channel
+// RegisterMessage registers client
+type RegisterMessage struct {
+	Message Message
+	Client  *websocket.Conn
+}
+
+type socketManager struct {
+	clients   map[string][]*websocket.Conn
+	register  chan RegisterMessage
+	broadcast chan Message
+}
+
+var manager = socketManager{
+	clients:   make(map[string][]*websocket.Conn),
+	register:  make(chan RegisterMessage), // registration channel
+	broadcast: make(chan Message),         // broadcast channel
+}
+
 var upgrader = websocket.Upgrader{}
 
 // HandleWs handles websocket connections
@@ -39,40 +55,59 @@ func HandleWs(w http.ResponseWriter, r *http.Request) {
 		}
 
 		if msg.Event == "register" {
-			log.Printf("Registering client for list: %s\n", msg.Data.ListID)
-			clients[msg.Data.ListID] = append(clients[msg.Data.ListID], ws)
+			registerMsg := RegisterMessage{
+				Message: msg,
+				Client:  ws,
+			}
+			manager.register <- registerMsg
+		} else {
+			manager.broadcast <- msg
 		}
-
-		broadcast <- msg
 	}
 }
 
 // HandleMessages processes WS messages and sends them to the client
 func HandleMessages() {
 	for {
-		msg := <-broadcast
-		listClients := clients[msg.Data.ListID]
-		log.Printf("Message for: %s", msg.Data.ListID)
-		log.Printf("Clients connected: %d\n", len(listClients))
-		for i, client := range listClients {
-			switch msg.Event {
-			case "register":
-				msg.Event = "register:success"
-				err := client.WriteJSON(msg)
-				if err != nil {
-					log.Printf("error: %v", err)
-					client.Close()
-					clients[msg.Data.ListID] = append(clients[msg.Data.ListID][:i], clients[msg.Data.ListID][i+1:]...)
-				}
-			case "todo:done":
-				err := client.WriteJSON(msg)
-				if err != nil {
-					log.Printf("error: %v", err)
-					client.Close()
-					clients[msg.Data.ListID] = append(clients[msg.Data.ListID][:i], clients[msg.Data.ListID][i+1:]...)
-				}
+		select {
+		case msg := <-manager.broadcast:
+			manager.handleMessage(&msg)
+		case reg := <-manager.register:
+			manager.registerClient(&reg)
+		}
+	}
+}
+
+func (m *socketManager) removeClient(listID string, c *websocket.Conn) {
+	listClients := m.clients[listID]
+	for i, client := range listClients {
+		if c == client {
+			m.clients[listID] = append(m.clients[listID][:i], m.clients[listID][i+1:]...)
+			break
+		}
+	}
+}
+
+func (m *socketManager) handleMessage(msg *Message) {
+	listClients := m.clients[msg.Data.ListID]
+	for _, client := range listClients {
+		switch msg.Event {
+		case "todo:done":
+			err := client.WriteJSON(msg)
+			if err != nil {
+				log.Printf("error: %v", err)
+				client.Close()
+				m.removeClient(msg.Data.ListID, client)
 			}
 		}
 	}
+}
 
+func (m *socketManager) registerClient(registerMsg *RegisterMessage) {
+	msg := registerMsg.Message
+	client := registerMsg.Client
+	log.Printf("Registering client for list: %s\n", msg.Data.ListID)
+	m.clients[msg.Data.ListID] = append(m.clients[msg.Data.ListID], client)
+	msg.Event = "register:success"
+	client.WriteJSON(msg)
 }
